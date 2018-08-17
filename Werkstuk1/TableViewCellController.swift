3ef97430e4288f69d48f0188dee5961d9c4e2639
FelixStruelens <felix.struelens@me.com>
2018-08-17 12:35:20 +0200

    ViewControllers toegevoegd

---
+++ Werkstuk1/TableViewCellController.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellController.swift
+//  Werkstuk1
+//
+//  Created by Felix Struelens on 17/08/18.
+//  Copyright © 2018 Felix Struelens. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
